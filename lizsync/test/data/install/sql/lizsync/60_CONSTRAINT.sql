fba3a0344298513977a8e0e52594df4a57f258c4
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-04-21 19:14:37 +0200

    run migrations on CI since 0.2.2

---
+++ lizsync/test/data/install/sql/lizsync/60_CONSTRAINT.sql
@@ -0,0 +1,49 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.16
+-- Dumped by pg_dump version 9.6.16
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+-- history history_pkey
+ALTER TABLE ONLY lizsync.history
+    ADD CONSTRAINT history_pkey PRIMARY KEY (sync_id);
+
+
+-- server_metadata server_metadata_pkey
+ALTER TABLE ONLY lizsync.server_metadata
+    ADD CONSTRAINT server_metadata_pkey PRIMARY KEY (server_id);
+
+
+-- server_metadata server_metadata_server_name_key
+ALTER TABLE ONLY lizsync.server_metadata
+    ADD CONSTRAINT server_metadata_server_name_key UNIQUE (server_name);
+
+
+-- synchronized_schemas synchronized_schemas_pkey
+ALTER TABLE ONLY lizsync.synchronized_schemas
+    ADD CONSTRAINT synchronized_schemas_pkey PRIMARY KEY (server_id);
+
+
+-- sys_structure_metadonnee sys_structure_metadonnee_pkey
+ALTER TABLE ONLY lizsync.sys_structure_metadonnee
+    ADD CONSTRAINT sys_structure_metadonnee_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- lizsync/test/test_load_structure.py
+++ lizsync/test/test_load_structure.py
@@ -1,5 +1,6 @@
 """Tests for Processing algorithms."""
 
+import os
 import psycopg2
 import time
 
@@ -17,6 +18,7 @@ else:
 
 from ..processing.provider import LizsyncProvider as ProcessingProvider
 from ..qgis_plugin_tools.tools.logger_processing import LoggerProcessingFeedBack
+from ..qgis_plugin_tools.tools.resources import metadata_config
 
 __copyright__ = "Copyright 2019, 3Liz"
 __license__ = "GPL version 3"
@@ -36,6 +38,89 @@ class TestProcessing(unittest.TestCase):
         del self.connection
         time.sleep(1)
 
+    def test_load_structure_with_migration(self):
+        """Test we can load the PostGIS structure with migrations."""
+        VERSION = "0.2.2"
+        provider = ProcessingProvider()
+        QgsApplication.processingRegistry().addProvider(provider)
+
+        feedback = LoggerProcessingFeedBack()
+        params = {
+            'CONNECTION_NAME_CENTRAL': 'test',
+            'OVERRIDE_AUDIT': True,  # Must be true, for the first time in the test.
+            'OVERRIDE_LIZSYNC': True,  # Must be true, for the first time in the test.
+        }
+
+        os.environ["DATABASE_RUN_MIGRATION"] = VERSION
+        try:
+            processing_output = processing.run(
+                "lizsync:create_database_structure", params, feedback=feedback
+            )
+        except QgsProcessingException as e:
+            self.assertTrue(False, e)
+        del os.environ["DATABASE_RUN_MIGRATION"]
+
+        self.cursor.execute(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema = 'lizsync'"
+        )
+        records = self.cursor.fetchall()
+        result = [r[0] for r in records]
+        # Expected tables in the specific version written above at the beginning of the test.
+        # DO NOT CHANGE HERE, change below at the end of the test.
+        expected = [
+            "history",
+            "server_metadata",
+            "synchronized_schemas",
+            "sys_structure_metadonnee",
+        ]
+        self.assertCountEqual(expected, result)
+        expected = "Lizsync database structure has been successfully created to version \"{}\".".format(VERSION)
+        self.assertEqual(expected, processing_output["OUTPUT_STRING"])
+
+        sql = '''
+            SELECT version
+            FROM lizsync.sys_structure_metadonnee
+            ORDER BY date_ajout DESC
+            LIMIT 1;
+        '''
+        self.cursor.execute(sql)
+        record = self.cursor.fetchone()
+        self.assertEqual(VERSION, record[0])
+
+        feedback.pushDebugInfo("Update the database")
+        params = {
+            "CONNECTION_NAME_CENTRAL": "test",
+            "RUNIT": True,
+        }
+        results = processing.run(
+            "lizsync:upgrade_database_structure", params, feedback=feedback
+        )
+        self.assertEqual(1, results["OUTPUT_STATUS"], 1)
+        metadata = metadata_config()
+        version = metadata["general"]["version"]
+        version = version.replace("-beta", "")
+        self.assertEqual(
+            "Lizsync database structure has been successfully upgraded to version \"{}\".".format(version),
+            results["OUTPUT_STRING"],
+        )
+
+        self.cursor.execute(sql)
+        record = self.cursor.fetchone()
+        self.assertEqual(version, record[0])
+
+        self.cursor.execute(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema = 'lizsync'"
+        )
+        records = self.cursor.fetchall()
+        result = [r[0] for r in records]
+        expected = [
+            "history",
+            "server_metadata",
+            "synchronized_schemas",
+            "sys_structure_metadonnee",
+        ]
+        self.assertCountEqual(expected, result)
+
     def test_load_structure_without_migrations(self):
         """Test we can load the PostGIS structure without migrations."""
         provider = ProcessingProvider()
@@ -52,8 +137,8 @@ class TestProcessing(unittest.TestCase):
 
         params = {
             'CONNECTION_NAME_CENTRAL': 'test',
-            'OVERRIDE_AUDIT': True,  # Must be true, for the time in the test.
-            'OVERRIDE_LIZSYNC': True,  # Must be true, for the time in the test.
+            'OVERRIDE_AUDIT': True,  # Must be true, for the first time in the test.
+            'OVERRIDE_LIZSYNC': True,  # Must be true, for the first time in the test.
         }
         processing.run("lizsync:create_database_structure", params, feedback=feedback)
 
