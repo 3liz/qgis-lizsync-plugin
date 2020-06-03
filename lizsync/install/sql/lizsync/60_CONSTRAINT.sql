2feb488cd7d609d0e963eec0cb8cb8987d0602d4
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-06-03 13:54:13 +0200

    Switch to PostgreSQL 10.6 for testing

--- lizsync/install/sql/lizsync/60_CONSTRAINT.sql
+++ lizsync/install/sql/lizsync/60_CONSTRAINT.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.17
--- Dumped by pg_dump version 9.6.17
+-- Dumped from database version 10.6 (Debian 10.6-1.pgdg90+1)
+-- Dumped by pg_dump version 10.6 (Debian 10.6-1.pgdg90+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -12,7 +12,6 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
-SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
