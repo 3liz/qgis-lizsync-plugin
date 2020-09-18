cd217f0d218483fa14df70bb56ce4282ecb9db49
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-09-18 14:22:06 +0200

    SQL - Update versions from the Postgis docker image

--- lizsync/install/sql/lizsync/60_CONSTRAINT.sql
+++ lizsync/install/sql/lizsync/60_CONSTRAINT.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 10.6 (Debian 10.6-1.pgdg90+1)
--- Dumped by pg_dump version 10.6 (Debian 10.6-1.pgdg90+1)
+-- Dumped from database version 10.14 (Debian 10.14-1.pgdg100+1)
+-- Dumped by pg_dump version 10.14 (Debian 10.14-1.pgdg100+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -12,6 +12,7 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
