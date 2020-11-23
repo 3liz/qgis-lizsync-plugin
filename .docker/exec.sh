a5ec3acf03614bce5cc2111a271babfdb5dd395d
Michael Douchin <mdouchin@3liz.com>
2020-11-23 10:06:47 +0100

    Tests - Wait for PosgreSQL before installing structure

--- .docker/exec.sh
+++ .docker/exec.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 export $(grep -v '^#' .env | xargs)
 
+echo "Test if PostgreSQL is ready"
+until docker exec postgis bash -c "psql service=test -c 'SELECT version()'" 1>  /dev/null 2>&1
+do
+  echo "."
+  sleep 1
+done
+echo "PostgreSQL is now ready !"
+
 docker exec -it postgis sh \
   -c "runuser -l postgres -c 'dropdb lizsync_clone_a && dropdb lizsync_clone_b'"
 docker exec -it postgis sh \
